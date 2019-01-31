23a6647823ff39cd3f1861843012e36be3b55290
Cheris Bakhishov <ccarey0228@gmail.com>
2019-01-30 21:03:59 -0800

    comment added

--- src/Calculator.java
+++ src/Calculator.java
@@ -20,11 +20,11 @@ public class Calculator {
 
         return num1 / num2;
     }
-
-    public static double divide(double num1, double num2) {
-
-        double result = num1 / num2;
-        return result;
-    }
+//THE ABOVE IS BETTER!
+//    public static double divide(double num1, double num2) {
+//
+//        double result = num1 / num2;
+//        return result;
+//    }
 
 }
